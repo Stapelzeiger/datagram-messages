1615f6aafe02dbf227eff6d2eb78c79424a1bcd4
Patrick Spieler <stapelzeiger@gmail.com>
2015-08-30 14:23:55 +0200

    python implementation

--- python/message.py
+++ python/message.py
@@ -1,5 +1,5 @@
 import msgpack
-
+import threading
 
 def encode(name, arg, msgpack_opt={'use_single_float': True}):
     """
@@ -14,3 +14,111 @@ def decode(datagram, msgpack_opt={'encoding': 'ascii'}):
     """
     msg = msgpack.unpackb(datagram, **msgpack_opt)
     return msg[0], msg[1]
+
+
+class ConnectionHandler:
+
+    class CallTimeout(Exception):
+        pass
+
+    def __init__(self, insock, outsock=None):
+        if outsock is None:
+            outsock = insock
+        self.insock = insock
+        self.outsock = outsock
+        self.sendlock = threading.Lock()
+        self.handlerlock = threading.Lock()
+        self.call_seq_nbr = 0
+        self.call_timeout = 0.5
+        self.call_nb_retries = 3
+        self.active_service_calls = {}
+        self.service_handlers = {}
+        self.message_handlers = {}
+
+    def set_msg_handler(self, msg, handler):
+        '''
+        register a message handler callback
+        the handler is called with the received message as argument
+        '''
+        with self.handlerlock:
+            self.message_handlers[msg] = handler
+
+    def set_service_handler(self, service, handler):
+        '''
+        register a service handler callback
+        the handler is called with the received message as argument and
+        and the return value is sent as the response
+        '''
+        with self.handlerlock:
+            self.service_handlers[service] = handler
+
+    def service_call(self, service, arg):
+        '''
+        perform a remote service call
+        '''
+        with self.handlerlock:
+            seq = self.call_seq_nbr
+            self.call_seq_nbr = seq + 1
+
+            res_signal = threading.Event()
+            self.active_service_calls[(service, seq)] = {'res': None, 'sig': res_signal}
+
+            nb_retries = self.call_nb_retries
+            timeout = self.call_timeout
+
+        for i in range(nb_retries):
+            with self.sendlock:
+                self.outsock.send(encode('req', [service, seq, arg]))
+            if res_signal.wait(timeout):
+                with self.handlerlock:
+                    return self.active_service_calls.pop((service, seq))['res']
+
+        raise self.CallTimeout('{} timed out. timeout {}s x {} retries'.format(service, timeout, nb_retries))
+
+    def send(self, name, msg):
+        '''
+        send a message
+        '''
+        if name in ('req', 'res'):
+            raise ValueError('reserved message name {}'.format(name))
+        with self.sendlock:
+            self.outsock.send(encode(name, msg))
+
+    def _handle_service_req(self, arg):
+        service_name, seq, request_arg = arg
+        with self.handlerlock:
+            if service_name in self.service_handlers:
+                res = self.service_handlers[service_name](request_arg)
+                res_datagram = encode('res', [service_name, seq, res])
+                with self.sendlock:
+                    self.outsock.send(res_datagram)
+
+    def _handle_service_res(self, arg):
+        service_name, seq, response_arg = arg
+        with self.handlerlock:
+            if (service_name, seq) in self.active_service_calls:
+                self.active_service_calls[(service_name, seq)]['res'] = response_arg
+                self.active_service_calls[(service_name, seq)]['sig'].set()
+
+    def _handle_message(self, name, msg):
+        with self.handlerlock:
+            if name in self.message_handlers:
+                self.message_handlers[name](msg)
+
+    def handle_datagram(self, datagram):
+        name, arg = decode(datagram)
+        if name == 'req':
+            self._handle_service_req(arg)
+        elif name == 'res':
+            self._handle_service_res(arg)
+        else:
+            self._handle_message(name, arg)
+
+    def spin_forever(self):
+        while True:
+            self.handle_datagram(self.insock.recv(1024))
+
+    def start_daemon(self):
+        t = threading.Thread(target=self.spin_forever)
+        t.daemon = True
+        t.start()
--- python/test/test_message.py
+++ python/test/test_message.py
@@ -1,6 +1,9 @@
 import unittest
 import message
 import msgpack
+import threading
+from unittest.mock import MagicMock, Mock, patch
+
 
 class MessageEncodeTestCase(unittest.TestCase):
 
@@ -8,8 +11,81 @@ class MessageEncodeTestCase(unittest.TestCase):
         self.assertEqual(message.encode('test', 42),
                          msgpack.packb(['test', 42]))
 
+
 class MessageDecodeTestCase(unittest.TestCase):
 
     def test_decode(self):
         self.assertEqual(message.decode(msgpack.packb(['test', 42])),
                          ('test', 42))
+
+
+class ConnectionHandlerTestCase(unittest.TestCase):
+
+    def test_send(self):
+        sock = Mock(spec=['send'])
+        s = message.ConnectionHandler(sock)
+        s.send('test', 'foo')
+        sock.send.assert_called_with(msgpack.packb(['test', 'foo']))
+
+    def test_send_reserved_name(self):
+        with self.assertRaisesRegex(ValueError, 'reserved message name req'):
+            message.ConnectionHandler(None).send('req', None)
+        with self.assertRaisesRegex(ValueError, 'reserved message name res'):
+            message.ConnectionHandler(None).send('res', None)
+
+    def test_handle_datagram(self):
+        s = message.ConnectionHandler(None)
+        s._handle_service_req = MagicMock()
+        s._handle_service_res = MagicMock()
+        s._handle_message = MagicMock()
+        s.handle_datagram(message.encode('req', ['test', 1234, 'args']))
+        s._handle_service_req.assert_called_once_with(['test', 1234, 'args'])
+        s.handle_datagram(message.encode('res', ['test', 1234, 'args']))
+        s._handle_service_res.assert_called_once_with(['test', 1234, 'args'])
+        s.handle_datagram(message.encode('test', 'foo'))
+        s._handle_message.assert_called_once_with('test', 'foo')
+
+    def test_handle_message(self):
+        s = message.ConnectionHandler(None)
+        handler = MagicMock()
+        s.set_msg_handler('test', handler)
+        s.handle_datagram(message.encode('test', 'foo'))
+        handler.assert_called_once_with('foo')
+
+    def test_handle_service(self):
+        sock = Mock(spec=['send'])
+        s = message.ConnectionHandler(sock)
+        handler = MagicMock(return_value=42)
+        s.set_service_handler('test', handler)
+        s.handle_datagram(message.encode('req', ['test', 1234, '6*9']))
+        handler.assert_called_once_with('6*9')
+        expected_result = message.encode('res', ['test', 1234, 42])
+        sock.send.assert_called_once_with(expected_result)
+
+    @patch.object(threading.Event, 'wait', autospec=True)
+    def test_service_call(self, event_wait_mock):
+        sock = Mock(spec=['send'])
+        s = message.ConnectionHandler(sock)
+
+        expected_req = message.encode('req', ['test', 0, 'arg'])
+        resp = message.encode('res', ['test', 0, 'result'])
+
+        def side_effect(self, timeout):
+            s.handle_datagram(resp)
+            return True
+
+        event_wait_mock.side_effect = side_effect
+
+        self.assertEqual(s.service_call('test', 'arg'), 'result')
+        sock.send.assert_called_once_with(expected_req)
+        # event_wait_mock.assert_called_once_with(0.5)
+
+    @patch.object(threading.Event, 'wait', autospec=True)
+    def test_service_call_timeout(self, event_wait_mock):
+        sock = Mock(spec=['send'])
+        s = message.ConnectionHandler(sock)
+
+        event_wait_mock.return_value = False
+        with self.assertRaisesRegex(message.ConnectionHandler.CallTimeout,
+                                    'servicename timed out. timeout 0.5s x 3 retries'):
+            s.service_call('servicename', 'arg')
