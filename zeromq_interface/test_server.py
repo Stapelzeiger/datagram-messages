4d19a9ff91d316db681c08a92fbf48fd63a93624
Patrick Spieler <stapelzeiger@gmail.com>
2015-10-09 22:04:31 +0200

    zeromq server test over serial-datagram

--- zeromq_interface/test_server.py
+++ zeromq_interface/test_server.py
@@ -21,23 +21,28 @@ call_handler.bind("tcp://*:5555")
 
 
 def publish_on_sock(sock, msg_name, content):
-    print('out: msg {}: {}'.format(msg_name, content))
+    # print('out: msg {}: {}'.format(msg_name, content))
     sock.send(msgpack.packb(msg_name) + msgpack.packb(content))
 
 msg_pub = lambda msg, content: publish_on_sock(msg_publisher_sock, msg, content)
 
 
-import socket
-UDP_IP_IN = "127.0.0.1"
-UDP_IP_OUT = "127.0.0.1"
-UDP_PORT_IN = 5006
-UDP_PORT_OUT = 5005
-sock_out = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock_out.connect((UDP_IP_OUT, UDP_PORT_OUT))
-sock_in = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock_in.bind((UDP_IP_IN, UDP_PORT_IN))
+# import socket
+# UDP_IP_IN = "127.0.0.1"
+# UDP_IP_OUT = "127.0.0.1"
+# UDP_PORT_IN = 5006
+# UDP_PORT_OUT = 5005
+# sock_out = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# sock_out.connect((UDP_IP_OUT, UDP_PORT_OUT))
+# sock_in = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# sock_in.bind((UDP_IP_IN, UDP_PORT_IN))
+# conn = datagrammessages.ConnectionHandler(sock_in, sock_out)
 
-conn = datagrammessages.ConnectionHandler(sock_in, sock_out)
+
+from datagrammessages.serialdatagramsocket import *
+sock = SerialDatagramSocket(sys.argv[1])
+conn = datagrammessages.ConnectionHandler(sock)
+
 conn.set_default_msg_handler(msg_pub)
 conn.start_daemon()
 
